4e7c09eade3066a0068bf7066677a4473fde72ed
Steph Nugs <steph.bowie1993@gmail.com>
2023-11-20 16:48:42 -0700

    added panels and panel views for indivdual panels and all 4 panels

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Dashboard from "./Dashboard";
+
+
+
+export default class Loading extends Component {
+
+    render() {
+        return <section className="loading">Loading</section>;
+    }
+}
